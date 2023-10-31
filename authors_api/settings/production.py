cc1f7b36a2e418a2b330bdac8c1e761bc0cdf3bd
Hosne Mubarak <h.m.tasmir.cse@gmail.com>
2023-10-31 14:27:01 +0600

    chore: setup besic django settings

---
+++ authors_api/settings/production.py
@@ -0,0 +1,8 @@
+from .base import * #noqa
+from .base import env
+
+ADMINS = [('Hosne Mubarak', 'h.m.tasmir.cse@gmail.com')]
+
+# TODO add domain  nnames of the production servers
+
+CSF_TRUSTED_ORIGIN =  ['']
